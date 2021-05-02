c7d07ab8c40bcbbc7534ecbdc867f5f5e2d22891
Isabella <isabellasantiago2469@gmail.com>
2021-05-02 12:07:51 -0700

    part 4

--- turnin/isant028_lab7_part4.c
+++ turnin/isant028_lab7_part4.c
@@ -23,21 +23,45 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00; 
-    DDRD = 0xFF; PORTD = 0x00; 
+    //DDRD = 0xFF; PORTD = 0x00; 
     /* Insert your solution below */
 	ADC_init();
 
 
-    //MAX VALUE: 1010001111 (655) from part 2 is to high without a flashlight so I am lowering it
-    int max = 300;
+    //MAX VALUE: 1010001111 (655) 
+    int max = 550;
     while (1) {
 	    unsigned short adc = ADC;
-        if (adc >= (max/2)){
-            PORTB = 0x01;
+        if (adc == max){
+            PORTB = 0xFF;
+        }
+        else if (adc >= max/2){
+            PORTB =0x7F; 
+        }
+         else if (adc >= max/3){
+            PORTB =0x3F; 
+        }
+         else if (adc >= max/4){
+            PORTB =0x1F; 
+        }
+         else if (adc >= max/5){
+            PORTB =0x0F; 
+        }
+         else if (adc >= max/6){
+            PORTB =0x07; 
+        }
+         else if (adc >= max/7){
+            PORTB =0x03; 
+        }
+        else if (adc >= max/8){
+            PORTB =0x01; 
         }
         else{
             PORTB = 0x00; 
         }
+
+        
+
     }
     return 1;
 
